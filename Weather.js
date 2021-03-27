e922b1eec48a56a4ab65304f1a7e96a813e29682
drdevlin <drdevlin@fastmail.com>
2021-03-27 16:37:03 -0400

    feat: throws when not given a string

--- Weather.js
+++ Weather.js
@@ -1,5 +1,7 @@
 class Weather {
   constructor(fetchedXMLWeatherData) {
+    if (typeof fetchedXMLWeatherData !== 'string') throw new TypeError('Argument must be a string.');
+    
     this._originalXML = fetchedXMLWeatherData;
   }
 }
