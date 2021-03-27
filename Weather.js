1896ba2fe4f0c33c7fc45d933dad4f5107af4ea7
drdevlin <drdevlin@fastmail.com>
2021-03-27 17:00:18 -0400

    feat: initial basic convert and store as object

--- Weather.js
+++ Weather.js
@@ -1,8 +1,11 @@
+const { xml2js } = require('xml-js');
+
 class Weather {
   constructor(fetchedXMLWeatherData) {
     if (typeof fetchedXMLWeatherData !== 'string') throw new TypeError('Argument must be a string.');
-    
+
     this._originalXML = fetchedXMLWeatherData;
+    this._data = xml2js(fetchedXMLWeatherData);
   }
 }
 
