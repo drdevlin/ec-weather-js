2a1b4d06b406dae2466edf424f96cc092ab12ad2
drdevlin <drdevlin@fastmail.com>
2021-03-27 19:11:32 -0400

    feat(private): convert and organize orig data

--- Weather.js
+++ Weather.js
@@ -7,6 +7,38 @@ class Weather {
     this._originalXML = fetchedXMLWeatherData;
     this._data = xml2js(fetchedXMLWeatherData);
   }
+
+  _convert(originalXML) {
+    const converted = xml2js(originalXML, { compact: true });
+
+    const mainData = converted.siteData;
+
+    return this._organize(mainData);
+  }
+
+  _organize(convertedObject) {
+    if (!Array.isArray(convertedObject)) {
+      // base case
+      const copy = { ...convertedObject };
+      let attributesAndValue = {};
+      if (copy.hasOwnProperty('_attributes')) {
+        attributesAndValue = { ...copy._attributes };
+        delete copy._attributes;
+      }
+      if (copy.hasOwnProperty('_text')) {
+        attributesAndValue.value = copy._text;
+        delete copy._text;
+      }
+      // recurse over other objects, including arrays
+      for (const prop in copy) {
+        if (typeof copy[prop] === 'object') copy[prop] = this._organize(copy[prop]);
+      }
+      return { ...attributesAndValue, ...copy };
+    } else {
+      // recurse over objects in array
+      return convertedObject.map(el => this._organize(el));
+    }
+  }
 }
 
 module.exports = Weather;
