e8065d2db19926c22869ed51411bf7f4001c83fa
drdevlin <drdevlin@fastmail.com>
2021-03-27 19:09:06 -0400

    test: for converting and organizing orig xml

--- Weather.test.js
+++ Weather.test.js
@@ -29,6 +29,45 @@ describe('Weather', () => {
       const weather = new Weather(fetchedData);
 
       expect(weather._data).toBeInstanceOf(Object);
+    });
+  });
+  describe('_convert(originalXML)', () => {
+    it('converts and organizes original fetched data', () => {
+      const fetchedData = `
+      <?xml version='1.0' encoding='ISO-8859-1'?>
+      <siteData>
+      <one attr1='foo' attr2='bar'>baz</one>
+      <two>bing</two>
+      <two>bang</two>
+      </siteData>
+      `;
+
+      const expected = {
+        one: { attr1: 'foo', attr2: 'bar', value: 'baz' },
+        two: [ { value: 'bing' }, { value: 'bang' } ]
+      }
+
+      const weather = new Weather(fetchedData);
+      console.log(weather._convert(fetchedData));
+
+      expect(weather._convert(fetchedData)).toMatchObject(expected);
+    });
+  });
+  describe('_organize(convertedObject)', () => {
+    it('recursively organizes objects converted by xml2js', () => {
+      const convertedObject = {
+        one: { _attributes: { attr1: 'foo', attr2: 'bar' }, _text: 'baz' },
+        two: [ { _text: 'bing' }, { _text: 'bang' } ]
+      };
+
+      const expected = {
+        one: { attr1: 'foo', attr2: 'bar', value: 'baz' },
+        two: [ { value: 'bing' }, { value: 'bang' } ]
+      };
+
+      const weather = new Weather(testdata);
+
+      expect(weather._organize(convertedObject)).toMatchObject(expected);
     })
   })
 })
