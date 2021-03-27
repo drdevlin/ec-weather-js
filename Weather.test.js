4f187e873addb45b99d9c0a74bf3ccd7f9da1f6e
drdevlin <drdevlin@fastmail.com>
2021-03-27 16:33:18 -0400

    test: for string only input

--- Weather.test.js
+++ Weather.test.js
@@ -7,6 +7,21 @@ describe('Weather', () => {
       const weather = new Weather(testdata);
 
       expect(weather._originalXML).toStrictEqual(testdata);
+    });
+    it('throws an Error if fetched data is not a string', () => {
+      const numberAttempt = () => {
+        const weather = new Weather(99);
+      }
+      const arrayAttempt = () => {
+        const weather = new Weather([ 9, 9 ]);
+      }
+      const objectAttempt = () => {
+        const weather = new Weather({ '9': 9 });
+      }
+
+      expect(numberAttempt).toThrow();
+      expect(arrayAttempt).toThrow();
+      expect(objectAttempt).toThrow();
     })
   })
 })
