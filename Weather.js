d8fa2d70a5726f2490c785209dc2d1f579580cda
drdevlin <drdevlin@fastmail.com>
2021-03-29 11:21:29 -0400

    doc: add JSDoc for Weather

--- Weather.js
+++ Weather.js
@@ -1,6 +1,22 @@
+/**
+ * A javascript API for Environment Canada's weather data.
+ * @module
+ */
+
 const Parse = require("./Parse");
 
+/**
+ * @classdesc A Weather instance has a number of methods for extracting specific types of data
+ * from the raw XML weather data fetched from Environment Canada.
+ * Thus, the XML data must first be fetched and then passed to the constructor.
+ */
 class Weather {
+
+  /**
+   * Parses and stores the XML data for use by the methods.
+   * Also, keeps a copy of the original data passed to it.
+   * @param {string} fetchedXMLWeatherData Raw XML weather data from Environment Canada.
+   */
   constructor(fetchedXMLWeatherData) {
     if (typeof fetchedXMLWeatherData !== 'string') throw new TypeError('Argument must be a string.');
 
@@ -8,10 +24,16 @@ class Weather {
     this._data = new Parse(this._originalXML).convert().normalize().simplify().restructure().data;
   }
 
+  /**
+   * @returns {Object} All weather data restructured into a javascript object.
+   */
   get all() {
     return this._data;
   }
 
+  /**
+   * @returns {string} Original XML weather data.
+   */
   get raw() {
     return this._originalXML;
   }
