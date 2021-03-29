92ec2d65b3cdcd0b008f3a83307d6765a78ab479
drdevlin <drdevlin@fastmail.com>
2021-03-28 21:39:50 -0400

    test: for getting all parsed data

--- Weather.test.js
+++ Weather.test.js
@@ -1,6 +1,46 @@
 const Weather = require('./Weather');
 const { testdata } = require('./testdata');
 
+const fetchedData = `
+  <?xml version='1.0' encoding='ISO-8859-1'?>
+  <siteData>
+  <license>text</license>
+  <dateTime></dateTime>
+  <dateTime></dateTime>
+  <one attr1='foo' attr2='bar'>baz</one>
+  <two>bing</two>
+  <two>bang</two>
+  <three><four></four></three>
+  <forecastGroup>
+    <regionalNormals>
+      <textSummary>text</textSummary>
+    </regionalNormals>
+    <forecast>
+      <period textForecastName="Today">Wednesday</period>
+      <textSummary>text</textSummary>
+    </forecast>
+    <forecast>
+      <period textForecastName="Tomorrow">Thursday</period>
+      <textSummary>text</textSummary>
+    </forecast>
+  </forecastGroup>
+  <hourlyForecastGroup>
+    <hourlyForecast dateTimeUTC="202101280300">
+      <condition>Rain</condition>
+    </hourlyForecast>
+    <hourlyForecast dateTimeUTC="202101280400">
+      <condition>Sunny</condition>
+    </hourlyForecast>
+  </hourlyForecastGroup>
+  </siteData>
+`;
+
+const forecastMap = new Map();
+forecastMap.set('Wednesday', { textSummary: 'text' });
+forecastMap.set('Thursday', { textSummary: 'text' });
+forecastMap.set('202101280300', { condition: 'Rain' });
+forecastMap.set('202101280400', { condition: 'Sunny' });
+
 describe('Weather', () => {
   describe('constructor(fetchedXMLWeatherData)', () => {
     it('stores the original fetched weather data', () => {
@@ -24,46 +64,6 @@ describe('Weather', () => {
       expect(objectAttempt).toThrow();
     });
     it('converts, parses, and stores weather data as an object', () => {
-      const fetchedData = `
-      <?xml version='1.0' encoding='ISO-8859-1'?>
-      <siteData>
-      <license>text</license>
-      <dateTime></dateTime>
-      <dateTime></dateTime>
-      <one attr1='foo' attr2='bar'>baz</one>
-      <two>bing</two>
-      <two>bang</two>
-      <three><four></four></three>
-      <forecastGroup>
-        <regionalNormals>
-          <textSummary>text</textSummary>
-        </regionalNormals>
-        <forecast>
-          <period textForecastName="Today">Wednesday</period>
-          <textSummary>text</textSummary>
-        </forecast>
-        <forecast>
-          <period textForecastName="Tomorrow">Thursday</period>
-          <textSummary>text</textSummary>
-        </forecast>
-      </forecastGroup>
-      <hourlyForecastGroup>
-        <hourlyForecast dateTimeUTC="202101280300">
-          <condition>Rain</condition>
-        </hourlyForecast>
-        <hourlyForecast dateTimeUTC="202101280400">
-          <condition>Sunny</condition>
-        </hourlyForecast>
-      </hourlyForecastGroup>
-      </siteData>
-      `;
-
-      const forecastMap = new Map();
-      forecastMap.set('Wednesday', { textSummary: 'text' });
-      forecastMap.set('Thursday', { textSummary: 'text' });
-      forecastMap.set('202101280300', { condition: 'Rain' });
-      forecastMap.set('202101280400', { condition: 'Sunny' });
-
       const expected = {
         one: { attr1: 'foo', attr2: 'bar', value: 'baz' },
         two: [ 'bing', 'bang' ],
@@ -77,4 +77,19 @@ describe('Weather', () => {
       expect(weather._data).toMatchObject(expected);
     });
   });
-})
+  describe('get all()', () => {
+    it('returns all data parsed', () => {
+      const expected = {
+        one: { attr1: 'foo', attr2: 'bar', value: 'baz' },
+        two: [ 'bing', 'bang' ],
+        three: { four: null },
+        regionalNormals: { textSummary: 'text' },
+        forecast: forecastMap
+      }
+
+      const weather = new Weather(fetchedData);
+
+      expect(weather.all).toMatchObject(expected);
+    });
+  });
+});
