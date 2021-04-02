4695d0aab0abb076166eb1837136c81afa108819
drdevlin <drdevlin@fastmail.com>
2021-04-01 21:00:22 -0400

    feat: retrieve precip forecasts

--- ForecastArray.js
+++ ForecastArray.js
@@ -2,12 +2,39 @@ class ForecastArray extends Array {
   get temperatures() {
     if (this[0].hasOwnProperty('day')) {
       return this.map(el => {
-        return { day: el.day, value: Number(el.temperatures.temperature.value) };
+        return { 
+          day: el.day,
+          value: Number(el.temperatures.temperature.value) 
+        };
       });
     }
     if (this[0].hasOwnProperty('hour')) {
       return this.map(el => {
-        return { hour: el.hour, value: Number(el.temperature.value) };
+        return { 
+          hour: el.hour, 
+          value: Number(el.temperature.value) 
+        };
+      });
+    }
+  }
+
+  get precipitation() {
+    if (this[0].hasOwnProperty('day')) {
+      return this.map(el => {
+        return {
+          day: el.day,
+          pop: (el.abbreviatedForecast.pop.value) ? Number(el.abbreviatedForecast.pop.value) : 0,
+          type: (el.precipitation.precipType.value) ? el.precipitation.precipType.value : null
+        };
+      });
+    }
+    if (this[0].hasOwnProperty('hour')) {
+      return this.map(el => {
+        return {
+          hour: el.hour,
+          condition: el.condition,
+          lop: (el.lop.value) ? Number(el.lop.value) : 0,
+        };
       });
     }
   }
